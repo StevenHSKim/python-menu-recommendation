b9666fc41fea6b8343a2927251ba315f29192823
Jyoung710 <youngyj0928@naver.com>
2024-06-03 23:19:03 +0900

    Modify crawling.py
    Add import tqdm

--- src/main/python/crawling.py
+++ src/main/python/crawling.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 from datetime import datetime
+import tqdm
 import json
 
 def crawl(radius):
